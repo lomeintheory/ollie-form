2d57d3062f88447fbafb993273291b06b0c1939f
Andrew Lo <al@iMac.lan>
2025-04-21 19:30:26 -0700

    set up form validation for email input field

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,35 @@
+'use client';
+
 import Image from 'next/image';
 import styles from './page.module.css';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { userSignUpSchema } from './validators/UserSignUpSchema';
+import { SubmitHandler, useForm } from 'react-hook-form';
+
+interface FormData {
+  email: string;
+  password: string;
+  confirmPassword: string;
+  petName: string;
+  petWeight: string;
+  idealPetWeight?: string;
+}
 
 export default function Home() {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(userSignUpSchema),
+    mode: 'onBlur',
+    reValidateMode: 'onBlur',
+  });
+
+  const formSubmit: SubmitHandler<FormData> = data => {
+    console.log(data);
+  };
+
   return (
     <div className={styles.page}>
       <main className={styles.mainContainer}>
@@ -15,13 +43,23 @@ export default function Home() {
             priority
           />
         </div>
-        <form className={styles.signUpForm}>
+        <form className={styles.signUpForm} onSubmit={handleSubmit(formSubmit)}>
+          <div className={styles.inputFieldContainer}>
+            <label htmlFor='email'>Email</label>
+            <input
+              type='email'
+              id='email'
+              placeholder='andrewlo957@gmail.com'
+              {...register('email')}
+            />
+            {errors.email && <div>{errors.email.message}</div>}
+          </div>
+
+          {/* <input />
           <input />
           <input />
           <input />
-          <input />
-          <input />
-          <input />
+          <input /> */}
           <button>Submit</button>
         </form>
       </main>
